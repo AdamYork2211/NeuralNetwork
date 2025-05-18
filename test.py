debe5d3f530110f9fa8343b04147b9a19542ff8d
AdamYork2211 <ak2211@york.ac.uk>
2025-05-18 20:11:32 +0100

    Update test.py
    also updated bottlenecking with Child Processes situation here

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ transform_test = transforms.Compose([
 test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                             download=True, transform=transform_test)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128,
-                                          shuffle=True, num_workers=2)
+                                          shuffle=True)
 
 class BasicBlock(nn.Module):
     expansion = 1 
