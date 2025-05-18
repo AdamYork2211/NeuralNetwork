dca91eccbfe7e2cd264109861fb417ed0e3cf44d
AdamYork2211 <ak2211@york.ac.uk>
2025-05-18 18:11:09 +0100

    Add files via upload

---
+++ train.py
@@ -0,0 +1,142 @@
+# Run the Train
+# It outputs a model.py
+# Run Test
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.optim as optim
+from collections import OrderedDict
+from torch.optim.lr_scheduler import StepLR
+
+device = torch.device("cpu")
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+
+batch_size = 128
+num_classes = 10
+learning_rate = 0.001
+epochs = 20
+
+
+transform_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+])
+
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                             download=True, transform=transform_train)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                           shuffle=True, num_workers=2)
+                                           
+class BasicBlock(nn.Module):
+    expansion = 1 
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample  # Used if dimensions change
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        identity = x
+
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+        return out
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10): #originally 1000
+        super(NeuralNet, self).__init__()
+        self.in_channels = 64
+        
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Create 4 layers of residual blocks
+        self.layer1 = self._make_layer(BasicBlock, 64,  2, stride=1) 
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        #self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(256 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride):
+        downsample = None
+
+        # If input and output dimensions differ, apply downsampling
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * block.expansion
+
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        #x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+criterion = nn.CrossEntropyLoss()
+
+model = NeuralNet(BasicBlock, [2, 2, 2, 2],num_classes=10).to(device) 
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=5e-4)
+scheduler = StepLR(optimizer, step_size=10, gamma=0.5)  # Halve LR every 10 epochs
+
+for epoch in range(epochs):
+    model.train()
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    scheduler.step()
+    print(f"Epoch [{epoch + 1}/{epochs}], Loss: {running_loss / len(train_loader):.4f}")
+
+torch.save(model.state_dict(), 'model.pth')
